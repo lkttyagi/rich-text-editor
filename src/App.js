618daa35092c28063ac83638bd9303e1924488ee
webiee <lktyagi76@gmail.com>
2019-12-18 19:01:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import MyEditor from './components/Editor';
+
+class App extends Component {
+  render() {
+    return <MyEditor />;
+  }
+}
+
+export default App;
